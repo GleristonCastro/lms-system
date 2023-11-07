c6d53c653e1b6ce2a6cd8f2150e716c7bb0605f9
Gleriston <xpanium@gmail.com>
2023-11-07 17:42:54 -0300

    add Category filter in the project

---
+++ app/(Home)/_components/CategoryFilter.jsx
@@ -0,0 +1,43 @@
+'use client'
+import { useState } from "react"
+
+function CategoryFilter() {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const filterOptions = [
+    {
+      id: 1,
+      name: 'All',
+      value: 'all'
+    },
+    {
+      id: 2,
+      name: 'React Js',
+      value: 'react'
+    },
+    {
+      id: 4,
+      name: 'Next Js',
+      value: 'nextjs'
+    },
+    {
+      id: 5,
+      name: 'Firebase',
+      value: 'firebase'
+    }
+  ]
+  return (
+    <div className='flex gap-5'>
+      {filterOptions.map((item,index)=>
+        <button
+          key={index}
+          className={`border p-2 px-4 text-sm rounded-md hover:border-purple-800 font-semibold hover:bg-gray-50 ${activeIndex == index ? 'border-purple-800 bg-purple-50 text-purple-800' : null}`}
+          onClick={() => setActiveIndex(index)}
+        >
+          <h2>{item.name}</h2>
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default CategoryFilter
