c38bd5ee0a791606958d270cb7a2b1e294c27abc
Gleriston <xpanium@gmail.com>
2023-11-07 17:52:01 -0300

    update: include get courses

--- app/(Home)/(routes)/browse/page.jsx
+++ app/(Home)/(routes)/browse/page.jsx
@@ -1,6 +1,24 @@
+'use client'
+
+import React, { useEffect } from 'react'
+import CategoryFilter from '../../_components/CategoryFilter'
+import { getCourseList } from '../../../_services/index'
+
 function Browse() {
+  useEffect(() => {
+    getCourses()
+  }, [])
+
+  const getCourses = () => {
+    getCourseList().then(resp => {
+      console.log(resp)
+    })
+  }
+
   return (
-    <div>Browse</div>
+    <div>
+      <CategoryFilter />
+    </div>
   )
 }
 
