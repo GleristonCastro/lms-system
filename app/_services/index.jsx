e7ae6082a07e54287ebf5c2e823ece2bd7b71ac6
Gleriston <xpanium@gmail.com>
2023-11-07 17:42:10 -0300

    add connection service whith hygraph API

---
+++ app/_services/index.jsx
@@ -0,0 +1,22 @@
+import request, { gql } from "graphql-request"
+
+const MASTER_URL =  'https://api-sa-east-1.hygraph.com/v2/'+process.env.NEXT_PUBLIC_HYGRAPH_KEY+'/master'
+
+export const getCourseList = async () => {
+  const query = gql`
+  query CourseLists {
+    courseLists {
+      name
+      banner {
+        url
+      }
+      free
+      id
+      totalChapters
+      tag
+    }
+  }
+  `
+  const result = await request (MASTER_URL, query)
+  return result
+}
